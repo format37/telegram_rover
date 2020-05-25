ae5865f09dc8160c7698bc281b400367ec84648f
Alex <format37@gmail.com>
2020-05-25 23:10:30 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -5,3 +5,21 @@ import busio
 #from adafruit_pca9685 import PCA9685
 from Adafruit_PCA9685 import PCA9685
 import RPi.GPIO as gpio
+
+i2c_bus = busio.I2C(SCL, SDA)
+pca = [
+	PCA9685(i2c_bus,address=0x40),
+	#PCA9685(i2c_bus,address=0x41)
+	]
+frequency = 1*2300
+direction = 1*0xffff
+for i in range(0,len(pca)):
+	pca[i].frequency = 60
+	pca[i].channels[0].duty_cycle = 0
+	pca[i].channels[1].duty_cycle = 0xffff
+	
+print('start')
+track=0
+pca[track].frequency = int(frequency)
+#pca[track].channels[1].duty_cycle = int(direction)
+pca[track].channels[0].duty_cycle = 0x7fff  #go
