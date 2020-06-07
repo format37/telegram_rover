c272517e6f6ec5196257c7fb70ec8422a70aa314
Alex <format37@gmail.com>
2020-06-07 22:46:21 +0300

    new file:   get_photo.py

---
+++ get_photo.py
@@ -0,0 +1,12 @@
+from picamera import PiCamera
+a=1920
+b=1080
+filepath='image.jpg'
+camera = PiCamera()
+camera.rotation=180
+camera.resolution = (int(a), int(b))#1920, 1080
+camera.start_preview()
+time.sleep(1)
+camera.capture(filepath)
+camera.stop_preview()
+camera.close()
