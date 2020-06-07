2434961e7bbca73b6221e8fc3569f7e5c95e0cae
Alex <format37@gmail.com>
2020-06-07 23:11:01 +0300

    modified:   get_photo.py

--- get_photo.py
+++ get_photo.py
@@ -1,10 +1,10 @@
 from picamera import PiCamera
 import time
 a=1920
-b=1080
+b=1920
 filepath='image.jpg'
 camera = PiCamera()
-camera.rotation=180
+#camera.rotation=180
 camera.resolution = (int(a), int(b))#1920, 1080
 camera.start_preview()
 time.sleep(1)
