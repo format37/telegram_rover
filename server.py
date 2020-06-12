47cd75d2c06121cb85c22df4ebf0ffa30acf3603
Alex <format37@gmail.com>
2020-06-12 13:08:06 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -3,18 +3,21 @@ from gpiozero import LED
 import Adafruit_PCA9685
 import time
 
-led = LED(23)
-track_free=[LED(17),LED(27)]
+led			= LED(23)
+track_free	= [LED(17),LED(27)]
+track_dir	= [LED(22),LED(24)]
 pwm = Adafruit_PCA9685.PCA9685()
 
-def track(pwm,t_free,channel,direction):
+def track(pwm,t_free,t_dir,channel,direction):
 	if channel==0:
 		if direction ==-1:
 			t_free[channel].off()#lock
+			t_dir[channel].off()#back
 		if direction == 0:
 			t_free[channel].on()#free
 		if direction == 1:
 			t_free[channel].off()#lock
+			t_dir[channel].on()#front
 	if channel==1:
 		if direction ==-1:
 			t_free[channel].off()#lock
@@ -24,18 +27,22 @@ def track(pwm,t_free,channel,direction):
 			t_free[channel].off()#lock
 
 print('init')
-track(pwm,track_free,channel=0,direction=0)
-track(pwm,track_free,channel=1,direction=0)
+track(pwm,track_free,track_dir,channel=0,direction=0)
+track(pwm,track_free,track_dir,channel=1,direction=0)
 pwm.set_pwm(0, 0, 3000)
 pwm.set_pwm(1, 0, 3000)
 						
 print('start')
-track(pwm,track_free,channel=0,direction=1)
-track(pwm,track_free,channel=1,direction=1)
+track(pwm,track_free,track_dir,channel=0,direction=1)
+#track(pwm,track_free,track_dir,channel=1,direction=1)
+time.sleep(3)
+track(pwm,track_free,track_dir,channel=0,direction=-1)
+#track(pwm,track_free,track_dir,channel=1,direction=-1)
 time.sleep(3)
 
+
 print('disable')
-track(pwm,track_free,channel=0,direction=0)
-track(pwm,track_free,channel=1,direction=0)
+track(pwm,track_free,track_dir,channel=0,direction=0)
+track(pwm,track_free,track_dir,channel=1,direction=0)
 while(True):
 	pass
