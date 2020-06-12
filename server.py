f7f5a2e5e49cc1fb9ede20deeab22e90c919cc73
Alex <format37@gmail.com>
2020-06-12 13:43:29 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 from gpiozero import LED
-import Adafruit_PCA9685
+#import Adafruit_PCA9685
+
+from board import SCL, SDA
+import busio
+from adafruit_pca9685 import PCA9685
+
 import time
 
+i2c_bus = busio.I2C(SCL, SDA)
+pca = PCA9685(i2c_bus,address=0x40)
+pca.frequency = 60
+pca.channels[0].duty_cycle = 0x7fff #go
+
+time.sleep(3)
+
+pca.channels[0].duty_cycle = 0 #stop
+
+'''
 led			= LED(23)
 track_free	= [LED(17),LED(27)]
 track_dir	= [LED(24),LED(22)]
-pwm = Adafruit_PCA9685.PCA9685()
+#pwm = Adafruit_PCA9685.PCA9685()
 
 def track(pwm,t_free,t_dir,channel,direction):
 	if channel==0:
@@ -35,11 +50,11 @@ track(pwm,track_free,track_dir,channel=1,direction=0)
 #pwm.set_pwm_freq(15000-5000)
 #pwm.set_pwm(0, 3000)
 #pwm.set_pwm(1, 3000)
-frequency = 100*2300
-pwm[0].frequency = int(frequency)
-pwm[1].frequency = int(frequency)
-pwm.channels[0].duty_cycle = 0x7fff
-pwm.channels[1].duty_cycle = 0x7fff
+#frequency = 100*2300
+#pwm[0].frequency = int(frequency)
+#pwm[1].frequency = int(frequency)
+#pwm.channels[0].duty_cycle = 0x7fff
+#pwm.channels[1].duty_cycle = 0x7fff
 						
 print('front')
 track(pwm,track_free,track_dir,channel=0,direction=1)
@@ -55,3 +70,4 @@ track(pwm,track_free,track_dir,channel=0,direction=0)
 track(pwm,track_free,track_dir,channel=1,direction=0)
 while(True):
 	pass
+'''
