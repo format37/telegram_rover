9a377a3f032b661dbea59a17f2c25cdc7633b44c
Alex <format37@gmail.com>
2020-06-12 11:26:50 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -11,12 +11,16 @@ def track(pwm,channel,power):
 	pwm.set_pwm(channel, 0, int(4000*power/100))
 	
 led = LED(23)
+tracks=[LED(17),LED(27)]
 pwm = Adafruit_PCA9685.PCA9685()
 
 led.off()
+tracks[0].on()
+tracks[1].on()
 track(pwm,0,100)
 track(pwm,1,100)
 time.sleep(3)
-led.on()
-track(pwm,0,0)
-track(pwm,1,0)
+#track(pwm,0,0)
+#track(pwm,1,0)
+tracks[0].off()
+tracks[1].off()
