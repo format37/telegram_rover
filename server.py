bb545b979109632e397a68455652b976668005cc
Alex <format37@gmail.com>
2020-06-12 14:38:15 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -11,11 +11,12 @@ import time
 print('init')
 i2c_bus = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c_bus)
-#speed	= 0.1
-#frequency	= speed*2300
+speed	= 0.9
+frequency	= speed*2300
 
-pca.frequency = 1400#int(frequency)
+pca.frequency = int(frequency)
 pca.channels[0].duty_cycle = 0x7fff #go
+pca.channels[1].duty_cycle = 0x7fff #go
 time.sleep(3)
 print('end')
 while(True):
