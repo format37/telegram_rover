eb78385f26f5c103d9f71e864750c3ca939ac82c
Alex <format37@gmail.com>
2020-06-22 22:54:13 +0300

    renamed:    get_voltage.py -> archive/get_voltage.py 	modified:   server.py

--- server.py
+++ server.py
@@ -39,65 +39,25 @@ def call_move(request):
 	frequency	= int( float(request.rel_url.query['speed'])*2300/100 )
 	frequency	= frequency if frequency>100 else 100
 	
-	print('init')
+	# init
 	track_power	= LED(23)
 	track_free	= [LED(27),LED(17)]
 	track_dir	= [LED(24),LED(22)]
 
 	i2c_bus = busio.I2C(SCL, SDA)
 	pca = PCA9685(i2c_bus)
-	#frequency	= 900 #speed 100-2300
 	pca.frequency=frequency
 	pca.channels[0].duty_cycle = 0x7fff #go
 	pca.channels[1].duty_cycle = 0x7fff #go
-
 	
-	#print('back')
-	
+	# move	
 	track(pca,track_free,track_dir,channel=0,direction=track_left)
 	track(pca,track_free,track_dir,channel=1,direction=track_right)
 	time.sleep(delay)
 	
-	'''
-	print('front')
-	track(pca,track_free,track_dir,channel=0,direction=1)
-	track(pca,track_free,track_dir,channel=1,direction=1)
-	time.sleep(1)
-
-	
-	print('front')
-	track(pca,track_free,track_dir,channel=0,direction=1)
-	track(pca,track_free,track_dir,channel=1,direction=1)
-	time.sleep(3)
-
-	print('back')
-	track(pca,track_free,track_dir,channel=0,direction=-1)
-	track(pca,track_free,track_dir,channel=1,direction=-1)
-	time.sleep(3)
-
-	print('stop')
+	# stop
 	track(pca,track_free,track_dir,channel=0,direction=0)
 	track(pca,track_free,track_dir,channel=1,direction=0)
-	time.sleep(3)
-
-	print('left')
-	track(pca,track_free,track_dir,channel=0,direction=-1)
-	track(pca,track_free,track_dir,channel=1,direction=1)
-	time.sleep(3)
-
-	print('right')
-	track(pca,track_free,track_dir,channel=0,direction=1)
-	track(pca,track_free,track_dir,channel=1,direction=-1)
-	time.sleep(3)
-'''
-	print('stop')
-	track(pca,track_free,track_dir,channel=0,direction=0)
-	track(pca,track_free,track_dir,channel=1,direction=0)
-'''
-	while(True):
-		time.sleep(1)
-		pass
-	'''
 
 async def call_check(request):
 	return web.Response(text='ok',content_type="text/html")
