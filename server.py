3ab5d8c30d6926dd6e2c0f8ec6114889dc4b4c06
Alex <format37@gmail.com>
2020-06-22 16:10:55 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -45,18 +45,18 @@ def move_back():
 	pca.channels[0].duty_cycle = 0x7fff #go
 	pca.channels[1].duty_cycle = 0x7fff #go
 
-	'''
+	
 	print('back')
 	track(pca,track_free,track_dir,channel=0,direction=-1)
 	track(pca,track_free,track_dir,channel=1,direction=-1)
-	time.sleep(6)
+	time.sleep(3)
 	'''
 	print('front')
 	track(pca,track_free,track_dir,channel=0,direction=1)
 	track(pca,track_free,track_dir,channel=1,direction=1)
 	time.sleep(1)
 
-	'''
+	
 	print('front')
 	track(pca,track_free,track_dir,channel=0,direction=1)
 	track(pca,track_free,track_dir,channel=1,direction=1)
