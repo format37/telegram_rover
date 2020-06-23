4aa94ccbd5f0d560c586886041a328ba2bdaff58
Alex <format37@gmail.com>
2020-06-24 00:12:41 +0300

    modified:   telegram_rover.py

--- telegram_rover.py
+++ telegram_rover.py
@@ -1,4 +1,5 @@
 import telebot
+import requests
 
 def bot_init(WEBHOOK_HOST,WEBHOOK_PORT,WEBHOOK_SSL_CERT, SCRIPT_PATH):
 
@@ -19,3 +20,14 @@ def bot_init(WEBHOOK_HOST,WEBHOOK_PORT,WEBHOOK_SSL_CERT, SCRIPT_PATH):
 	print(WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
 
 	return bot
+
+def move_cmd(cmd):
+	headers = {
+    "Origin": "http://95.165.139.53",
+    "Referer": "http://95.165.139.53/",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
+	
+	#cmd = 'track_left=-1&track_right=-1&delay=5&speed=10'
+	url	= "http://95.165.139.53:8823/telegram_rover.php?"+cmd
+	#+urllib.parse.quote_plus(message)
+	requests.get(url,headers = headers)
