b6547c8ccf8e205d2d498007a9292e3202a1796d
Alex <format37@gmail.com>
2020-06-24 10:25:40 +0300

    modified:   telegram_rover.py

--- telegram_rover.py
+++ telegram_rover.py
@@ -21,13 +21,41 @@ def bot_init(WEBHOOK_HOST,WEBHOOK_PORT,WEBHOOK_SSL_CERT, SCRIPT_PATH):
 
 	return bot
 
-def move_cmd(cmd):
+def move_cmd(user_id,cmd):
+	god_mode	= user_id == 106129214
+	if god_mode:
+		url	= "http://95.165.139.53:8823/telegram_rover.php?"+cmd
+		requests.get(url,headers = headers)
+		return 'job complete: '+cmd
+	else:
+		return "unavailable for. u"
+
+	'''
 	headers = {
-    "Origin": "http://95.165.139.53",
-    "Referer": "http://95.165.139.53/",
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
-	
+		"Origin": "http://95.165.139.53",
+		"Referer": "http://95.165.139.53/",
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
+	'''
 	#cmd = 'track_left=-1&track_right=-1&delay=5&speed=10'
 	url	= "http://95.165.139.53:8823/telegram_rover.php?"+cmd
-	#+urllib.parse.quote_plus(message)
 	requests.get(url,headers = headers)
+
+def move_f(user_id,delay,speed):
+	cmd 	= 'track_left=1&track_right=1&delay='+str(delay)+'&speed='+str(speed)
+	url	= "http://95.165.139.53:8823/telegram_rover.php?"+cmd
+	return	requests.get(url,headers = headers)
+	
+def move_b(user_id,delay,speed):
+	cmd 	= 'track_left=-1&track_right=-1&delay='+str(delay)+'&speed='+str(speed)
+	url	= "http://95.165.139.53:8823/telegram_rover.php?"+cmd
+	return	requests.get(url,headers = headers)
+	
+def move_l(user_id,delay,speed):
+	cmd 	= 'track_left=-1&track_right=1&delay='+str(delay)+'&speed='+str(speed)
+	url	= "http://95.165.139.53:8823/telegram_rover.php?"+cmd
+	return	requests.get(url,headers = headers)
+	
+def move_r(user_id,delay,speed):
+	cmd 	= 'track_left=1&track_right=-1&delay='+str(delay)+'&speed='+str(speed)
+	url	= "http://95.165.139.53:8823/telegram_rover.php?"+cmd
+	return	requests.get(url,headers = headers)
