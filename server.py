d74ef0090fb8c73482741171a1f522b20f4ce924
Rover alex <format37@gmail.com>
2020-07-17 21:01:12 +0100

    modified:   image.jpg 	modified:   server.py

--- server.py
+++ server.py
@@ -65,12 +65,12 @@ def call_move(request):
 async def call_photo(request):
 	
 	# get photo
-	#a=1920
-	#b=1920
+	a=1920
+	b=1920
 	#a=640
 	#b=480
-	a=320
-	b=240
+	#a=320
+	#b=240
 	filepath='/home/pi/telegram_rover/image.jpg'
 	camera = PiCamera()
 	#camera.rotation=180
