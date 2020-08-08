569f3009325559916abb01dbe109d2b62d29dc83
Alex <format37@gmail.com>
2020-08-08 08:23:23 +0300

    modified:   send_video.py

--- capture/send_video.py
+++ capture/send_video.py
@@ -11,50 +11,35 @@ def send_to_telegram():
 	video_file = open('/home/pi/telegram_rover/capture/mp4/out.mp4', 'rb')
 	main_bot.send_video('-384403215', video_file,timeout=10)
 	
-#converting
-mp4_files = []
-script_path = '/home/pi/telegram_rover/capture/'
-for root, subdirs, files in os.walk(script_path+'h264/'):
-	for filename in files:
-		
-		#cmd = 'ffmpeg -framerate 24 -i '+script_path+'h264/'+filename+' -c copy '+script_path+'mp4/'+filename[:-5]+'.mp4'
-		
-		cmd = [ 'ffmpeg',
-					'-framerate', '24',
-					'-i',
-					script_path+'h264/'+filename,
-					'-c','copy',
-					script_path+'mp4/'+filename[:-5]+'.mp4'
-			  ]
-		ffmpeg = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
-		out, err = ffmpeg.communicate()
+def convert():
+	mp4_files = []
+	h264_files = []
+	script_path = '/home/pi/telegram_rover/capture/'
+	for root, subdirs, files in os.walk(script_path+'h264/'):
+		for filename in files:		
+			cmd = [ 'ffmpeg',
+						'-framerate', '24',
+						'-i',
+						script_path+'h264/'+filename,
+						'-c','copy',
+						script_path+'mp4/'+filename[:-5]+'.mp4'
+				  ]
+			ffmpeg = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+			out, err = ffmpeg.communicate()
+			mp4_files.append(script_path+'mp4/'+filename[:-5]+'.mp4')
+			h264_files.append(script_path+'h264/'+filename)
+	return mp4_files
 
-		if(err) : print('error',err)
-		
-		#params = []
-		#params.append('ffmpeg')
-		#params.append('-framerate 24')
-		#params.append('24')
-		#params.append('-i')
-		#params.append(script_path+'h264/'+filename)
-		#params.append('-c')
-		#params.append('copy')
-		#params.append(script_path+'mp4/'+filename[:-5]+'.mp4')
-		
-		#cmd = 'ffmpeg -framerate 24 -i '+script_path+'h264/'+filename+' -c copy '+script_path+'mp4/'+filename[:-5]+'.mp4'
-		
-		mp4_files.append(script_path+'mp4/'+filename[:-5]+'.mp4')
-		
-		#print('ffmpeg'+cmd)
-		#MyOut = subprocess.Popen(
-		#	cmd,
-		#	stdout=subprocess.PIPE, 
-		#	stderr=subprocess.STDOUT
-		#)
-		#stdout,stderr = MyOut.communicate()
-		#for line in stdout:
-		#	print(line)
-		#process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
-		#for line in process.stdout:
-		#	print(line)
-print('k')
+def merge(mp4_files):
+	script_path = '/home/pi/telegram_rover/capture/'
+	cmd = [ 'MP4Box','-force-cat']
+	for full_path in mp4_files:
+		#MP4Box -force-cat -cat output0.mp4 -cat output1.mp4 out.mp4
+		cmd.append('-cat')
+		cmd.append(full_path)
+	cmd.append(script_path+'out.mp4')
+	MP4Box = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+	out, err = MP4Box.communicate()
+
+h264_files,mp4_files = convert()
+merge(mp4_files)
