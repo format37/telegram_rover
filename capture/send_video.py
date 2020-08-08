2b023b4db7ca76d355148c592571c1ae33fbad3f
Alex <format37@gmail.com>
2020-08-08 06:44:59 +0300

    modified:   send_video.py

--- capture/send_video.py
+++ capture/send_video.py
@@ -5,5 +5,5 @@ with open('/home/pi/telegram_rover/token.key','r') as file:
 	file.close()
 main_bot = telebot.TeleBot(MAIN_API_TOKEN)
 video_file = open('/home/pi/telegram_rover/capture/mp4/out.mp4', 'rb')
-main_bot.send_video('-384403215', video_file)
+main_bot.send_video('-384403215', video_file,timeout=10)
 #main_bot.send_video('-384403215', 'https://www.scriptlab.net/telegram/bots/calcubot/help.mp4')
