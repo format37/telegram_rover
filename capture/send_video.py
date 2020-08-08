32caba62f03b8b87729cf7cf2db56a7fedede737
Alex <format37@gmail.com>
2020-08-08 08:06:57 +0300

    modified:   send_video.py

--- capture/send_video.py
+++ capture/send_video.py
@@ -16,6 +16,21 @@ mp4_files = []
 script_path = '/home/pi/telegram_rover/capture/'
 for root, subdirs, files in os.walk(script_path+'h264/'):
 	for filename in files:
+		
+		#cmd = 'ffmpeg -framerate 24 -i '+script_path+'h264/'+filename+' -c copy '+script_path+'mp4/'+filename[:-5]+'.mp4'
+		
+		cmd = [ 'ffmpeg',
+					'-framerate', '24',
+					'-i',
+					script_path+'h264/'+filename,
+					'-c','copy',
+					script_path+'mp4/'+filename[:-5]+'.mp4'
+			  ]
+		ffmpeg = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+		out, err = ffmpeg.communicate(frames.tostring() )
+
+		if(err) : print('error',err)
+		
 		#params = []
 		#params.append('ffmpeg')
 		#params.append('-framerate 24')
@@ -25,8 +40,11 @@ for root, subdirs, files in os.walk(script_path+'h264/'):
 		#params.append('-c')
 		#params.append('copy')
 		#params.append(script_path+'mp4/'+filename[:-5]+'.mp4')
-		cmd = 'ffmpeg -framerate 24 -i '+script_path+'h264/'+filename+' -c copy '+script_path+'mp4/'+filename[:-5]+'.mp4'
+		
+		#cmd = 'ffmpeg -framerate 24 -i '+script_path+'h264/'+filename+' -c copy '+script_path+'mp4/'+filename[:-5]+'.mp4'
+		
 		mp4_files.append(script_path+'mp4/'+filename[:-5]+'.mp4')
+		
 		#print('ffmpeg'+cmd)
 		#MyOut = subprocess.Popen(
 		#	cmd,
@@ -36,7 +54,7 @@ for root, subdirs, files in os.walk(script_path+'h264/'):
 		#stdout,stderr = MyOut.communicate()
 		#for line in stdout:
 		#	print(line)
-		process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
-		for line in process.stdout:
-			print(line)
+		#process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
+		#for line in process.stdout:
+		#	print(line)
 print('k')
