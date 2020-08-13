44abe7172fd8ad4defcf08f32fc2acabf8bdfb3c
Alex <format37@gmail.com>
2020-08-13 20:56:15 +0300

    new file:   charge_mode.key 	new file:   charger.py 	modified:   volt_mon.py

---
+++ charger.py
@@ -0,0 +1,49 @@
+from ina219 import INA219
+import time
+
+def send_report(message):
+	with open('/home/pi/telegram_rover/token.key','r') as file:
+		MAIN_API_TOKEN=file.read().replace('\n', '')
+		file.close()
+	main_bot = telebot.TeleBot(MAIN_API_TOKEN)
+	main_bot.send_message('-384403215', message)
+	
+def move_forward():
+	
+	frequency	= 2300
+	
+	# init
+	track_power	= LED(23)	
+	track_free	= [LED(27),LED(17)]
+	track_dir	= [LED(24),LED(22)]
+
+	i2c_bus = busio.I2C(SCL, SDA)
+	pca = PCA9685(i2c_bus)
+	pca.frequency=frequency
+	pca.channels[0].duty_cycle = 0x7fff #go
+	pca.channels[1].duty_cycle = 0x7fff #go
+	
+	# move	
+	track(pca,track_free,track_dir,channel=0,direction=1)
+	track(pca,track_free,track_dir,channel=1,direction=1)	
+	time.sleep(2)
+	
+	# stop
+	track(pca,track_free,track_dir,channel=0,direction=0)
+	track(pca,track_free,track_dir,channel=1,direction=0)
+
+with open('/home/pi/telegram_rover/charge_mode.key','r') as file:
+	charge_mode=int(file.read().replace('\n', ''))
+	file.close()	
+
+if charge_mode:
+	SHUNT_OHMS = 0.1
+	ina = INA219(SHUNT_OHMS)
+	ina.configure()		
+	voltage_current = ina.voltage()
+	if voltage_current<13:
+		send_report('low voltage: '+str(voltage_current)+' v. Moving forward..')
+		move_forward()
+		time.sleep(1)
+		voltage_current = ina.voltage()
+		send_report('Voltage current: '+str(voltage_current))
--- volt_mon.py
+++ volt_mon.py
@@ -1,5 +1,13 @@
+from adafruit_pca9685 import PCA9685
 from ina219 import INA219
 import time
+
+def send_report(voltage):
+	with open('/home/pi/telegram_rover/token.key','r') as file:
+		MAIN_API_TOKEN=file.read().replace('\n', '')
+		file.close()
+	main_bot = telebot.TeleBot(MAIN_API_TOKEN)
+	main_bot.send_message('-384403215', 'low voltage: '+str(voltage)+' v. Moving forward..')
 
 SHUNT_OHMS = 0.1
 ina = INA219(SHUNT_OHMS)
@@ -7,3 +15,4 @@ ina.configure()
 while True:	
 	print(str(round(ina.voltage(),2))+" v. ")
 	time.sleep(0.5)
+	
